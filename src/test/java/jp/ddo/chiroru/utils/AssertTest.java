47e8569f2c9ebad4346a29b2a1451a449e44b442
Shinichi Hatano <shin@Shinichi-no-MacBook-Air.local>
2013-07-09 01:28:40 +0900

    Assertのテストを追加.

--- src/test/java/jp/ddo/chiroru/utils/AssertTest.java
+++ src/test/java/jp/ddo/chiroru/utils/AssertTest.java
@@ -1,6 +1,14 @@
 package jp.ddo.chiroru.utils;
 
+import org.junit.Test;
 
 public class AssertTest {
 
+    // Theoryを利用
+    @Test(expected = IllegalArgumentException.class)
+    public void nullを許可しないことを表明()
+            throws Exception {
+        Assert.isNotBlank("test");
+        Assert.isNotBlank(null);
+    }
 }
