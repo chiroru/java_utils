729bb98a121cd8a39aaa160829cf75678fcc0eb1
chiroru <chiroru_0130@yahoo.co.jp>
2013-07-12 00:03:19 +0900

    DBUnitを拡張するためJUnit4.xのTestRuleを実装.

---
+++ src/main/java/jp/ddo/chiroru/test/unit/DBUnitTestRule.java
@@ -0,0 +1,102 @@
+package jp.ddo.chiroru.test.unit;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.dbunit.AbstractDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.ext.h2.H2DataTypeFactory;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public abstract class DBUnitTestRule
+        extends AbstractDatabaseTester
+        implements TestRule {
+
+    private final String connectionUrl;
+    private final String username;
+    private final String password;
+
+    public DBUnitTestRule(String driverClass, String connectionUrl) {
+        this(driverClass, connectionUrl, null, null);
+    }
+    
+    public DBUnitTestRule(String driverClass,
+            String connectionUrl,
+            String username,
+            String password) {
+        this(driverClass, connectionUrl, username, password, null);
+    }
+    
+    public DBUnitTestRule(String driverClass,
+                          String connectionUrl,
+                          String username,
+                          String password,
+                          String schema) {
+        super(schema);
+        this.connectionUrl = connectionUrl;
+        this.username = username;
+        this.password = password;
+        assertNotNullNorEmpty("driverClass", driverClass);
+        try {
+            Class.forName(driverClass);
+        } catch (ClassNotFoundException e) {
+            throw new AssertionError(e);
+        }
+    }
+    
+    @Override
+    public IDatabaseConnection getConnection() throws Exception {
+        Connection conn = null;
+        if (username == null && password == null) {
+            conn = DriverManager.getConnection(connectionUrl);
+        } else {
+            conn = DriverManager.getConnection(connectionUrl,username, password);
+        }
+        DatabaseConnection dbConnection = new DatabaseConnection(conn, getSchema());
+        DatabaseConfig config = dbConnection.getConfig();
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new H2DataTypeFactory());
+        return null;
+    }
+
+    protected void executeQuery(String sql) throws Exception {
+        Connection conn = getConnection().getConnection();
+        conn.createStatement().execute(sql);
+        conn.commit();
+        conn.close();
+    }
+    
+    abstract protected IDataSet createDataSet() throws Exception;
+    
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        
+        return new Statement() {
+          @Override
+          public void evaluate() throws Throwable {
+              before();
+              setDataSet(createDataSet());
+              onSetup();
+              try {
+                  base.evaluate();
+              } finally {
+                  try {
+                      after();
+                  } finally {
+                      onTearDown();
+                  }
+              }
+          }
+        };
+    }
+
+    protected void before() throws Exception {
+    }
+    
+    protected void after() throws Exception {
+    }
+}
