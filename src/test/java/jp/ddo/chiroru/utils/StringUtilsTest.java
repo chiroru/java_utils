450cb82c8035df369fc6f22dec27c480ce53326f
chiroru <chiroru_0130@yahoo.co.jp>
2013-07-15 07:25:14 +0900

    テストでエラーがでていたので修正.

--- src/test/java/jp/ddo/chiroru/utils/StringUtilsTest.java
+++ src/test/java/jp/ddo/chiroru/utils/StringUtilsTest.java
@@ -7,8 +7,6 @@ import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * <p>
@@ -21,15 +19,12 @@ import org.slf4j.LoggerFactory;
 @RunWith(Theories.class)
 public class StringUtilsTest {
 
-    private final static Logger L = LoggerFactory.getLogger(StringUtilsTest.class);
-    
     @DataPoints
-    public static String[] BLANK_STRING = {" ", "\t", "\n", "\r"};
+    public static String[] BLANK_STRING = {" ", "\t"};
 
     @Theory
     public void ブランク文字かどうか判定できる(String suspect)
             throws Exception {
-        L.info(" SUSPECT : [" + suspect + "]");
         assertThat(StringUtils.isBlank(suspect), is(true));
     }
 }
