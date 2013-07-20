81e97f25fae86e345d3384ac3068e4ad7ad90d34
chiroru <chiroru_0130@yahoo.co.jp>
2013-07-21 01:31:20 +0900

    Assertクラスのテストを追加.

--- src/test/java/jp/ddo/chiroru/utils/AssertTest.java
+++ src/test/java/jp/ddo/chiroru/utils/AssertTest.java
@@ -1,14 +1,40 @@
 package jp.ddo.chiroru.utils;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
 public class AssertTest {
 
-    // Theoryを利用
-    @Test(expected = IllegalArgumentException.class)
-    public void nullを許可しないことを表明()
-            throws Exception {
-        Assert.isNotBlank("test");
-        Assert.isNotBlank(null);
+    @RunWith(Theories.class)
+    public static class 表明違反にならない {
+
+        @DataPoints
+        public static Object[] NOT_NULL_VALUES = {
+            123, "test", "\n"
+        };
+
+        @Theory
+        public void nullを許可しない(Object suspect) {
+            System.out.println(suspect);
+            Assert.isNotNull(suspect);
+            assertTrue(true);
+        }
+    }
+
+    public static class 表明違反 {
+        // Theoryを利用
+        @Test(expected = IllegalArgumentException.class)
+        public void nullを許可しないことを表明()
+                throws Exception {
+            Assert.isNotBlank("test");
+            Assert.isNotBlank(null);
+        }
     }
 }
